cc13074969ddba9d1a057e9a231a2549f70bc6e2
moriya shige <oumiya.s676@gmail.com>
2021-10-04 13:02:55 +0900

    自機や敵の位置・サイズ関係を調整

--- script/CSGBox.gd
+++ script/CSGBox.gd
@@ -6,6 +6,8 @@ export (PackedScene) var bullet
 # var b = "text"
 export var speed = 16
 
+const _x_limit = 6.4
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,11 +18,13 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		translation.x -= delta * speed
+		if translation.x < _x_limit * -1:
+			translation.x = _x_limit * -1
 	if Input.is_action_pressed("ui_right"):
 		translation.x += delta * speed
+		if translation.x > _x_limit:
+			translation.x = _x_limit
 	if Input.is_action_just_pressed("shot"):
 		var b = bullet.instance()
 		b.translation = global_transform.origin
-#		b.translation.z = get_parent().translation.z
 		get_parent().get_parent().add_child(b)
-		print(b.translation)
--- script/spawner.gd
+++ script/spawner.gd
@@ -1,28 +1,16 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var box2_scene = preload("res://scene/CSGBox2.tscn")
-
-# Called when the node enters the scene tree for the first time.
+var rng = RandomNumberGenerator.new()
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	rng.randomize()
 
 func _on_Timer_timeout():
 	var temp = box2_scene.instance()
-	var cam = get_node("../ClippedCamera")
+	var cam = get_node("../Player")
 	get_parent().add_child(temp)
 	temp.translation = cam.translation
+	temp.translation.x += rng.randf_range(-6.4, 6.4)
+	temp.translation.y = 0
 	temp.translation.z -= 40
-	temp.translation.x = randi() % 41 - 20
-
-	pass # Replace with function body.
+	print(temp.translation)
